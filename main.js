a8f48773aa0a24b45f2bc5fb624b208c78c62659
Viraj Chandra <virajchandra51@gmail.com>
2023-09-07 20:45:49 +0530

    new saber

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".hamburger");
+const container = document.querySelector(".pre-container");
 const lineOne = document.querySelector(".l-1");
 const lineTwo = document.querySelector(".l-2");
 const lineThree = document.querySelector(".l-3");
@@ -7,127 +7,134 @@ let a1 = {},
   a2 = {},
   a3 = {};
 
-hamburger.addEventListener("click", () => {
+container.addEventListener("click", () => {
   isOpen = !isOpen;
   if (isOpen) return openMenu();
   return closeMenu();
 });
 
+function initial() {
+  a1 = gsap
+    .timeline()
+    .to(lineOne, {
+      rotate: 80,
+      x: -200,
+      y: 10,
+      background: "yellow",
+    })
+    
+
+  a2 = gsap
+    .timeline()
+    .to(lineThree, {
+      rotate: 100,
+      background: "white",
+      x: 200,
+      y: -6,
+    })
+    
+}
+
+initial();
+
 function openMenu() {
   a1 = gsap
     .timeline()
     .to(lineOne, {
-      rotate: 90,
-      x: -12,
+      rotate: 130,
+      x: -5,
       y: 10,
-      background: "green"
     })
     .to(lineOne, {
       rotate: 130,
       x: -5,
-      y: 10
+      y: 10,
     })
     .to(lineOne, {
       rotate: 130,
       x: -15,
-      y: 10
+      y: 10,
     })
     .to(
       lineOne,
       {
         rotate: 100,
         x: -7,
-        y: 10
+        y: 10,
       },
       "-=0.25"
     )
     .to(lineOne, {
       rotate: 110,
       x: -25,
-      y: 10
+      y: 10,
     })
     .to(lineOne, {
       rotate: 130,
-      x: 0
+      x: 0,
     })
     .to(lineOne, {
       rotate: 135,
       y: 10,
       x: 0,
-      background: "black",
-      delay: 0.4
+      delay: 0.4,
     });
 
   a2 = gsap
     .timeline()
     .to(lineThree, {
-      rotate: 90,
-      background: "red",
-      x: 12,
-      y: -7
-    })
-    .to(lineThree, {
       rotate: 50,
       x: 20,
-      y: -3
+      y: 0,
     })
     .to(
       lineThree,
       {
         rotate: 80,
         x: 0,
-        y: -7,
-        duration: 0.25
+        y: 0,
+        duration: 0.25,
       },
       "-=0.25"
     )
     .to(lineThree, {
       rotate: 50,
       x: 17,
-      y: -10
+      y: 0,
     })
     .to(lineThree, {
       rotate: 50,
       x: -20,
-      y: -10
+      y: 0,
     })
     .to(lineThree, {
       rotate: 420,
       y: -9,
-      x: 5
+      x: 5,
     })
     .to(lineThree, {
       rotate: 45,
-      y: -8,
+      y: -6,
       x: 0,
-      background: "black",
-      delay: 0.4
+      delay: 0.4,
     });
-
-  a3 = gsap.to(lineTwo, {
-    opacity: 0
-  });
 }
 
 function closeMenu() {
   a1.kill();
   a2.kill();
   a3.kill();
-
   gsap.to(lineOne, {
     rotate: 0,
     background: "black",
     x: 0,
-    y: 0
+    y: 0,
   });
   gsap.to(lineThree, {
     rotate: 0,
     x: 0,
     background: "black",
-    y: 0
+    y: 0,
   });
 
-  gsap.to(lineTwo, {
-    opacity: 1
-  });
 }
